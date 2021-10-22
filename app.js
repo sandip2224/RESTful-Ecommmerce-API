e2bc70d66f51c6cbfe8c8ce56fde21a4df0bb79c
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2021-10-22 15:49:21 +0530

    add express rate limiter on app.js
    close #6

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
-const express = require('express')
-const colors = require('colors')
-const path = require('path')
-require('dotenv').config({ path: './api/config/config.env' })
+const express = require("express");
+const colors = require("colors");
+const path = require("path");
+const rateLimit = require("express-rate-limit");
+require("dotenv").config({ path: "./api/config/config.env" });
 
-const orderModel = require('./api/models/Order')
+const orderModel = require("./api/models/Order");
 
-const connectDB = require('./api/config/db')
+const connectDB = require("./api/config/db");
 
-const app = express()
+const app = express();
 
 // Connect to database
-connectDB()
+connectDB();
 
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Mounting routes
-app.use('/api/products', require('./api/routes/productRoute'))
-app.use('/api/orders', require('./api/routes/orderRoute'))
-app.use('/api/users', require('./api/routes/userRoute'))
-app.use('/api/payments', require('./api/routes/paymentRoute'))
+app.use("/api/products", require("./api/routes/productRoute"));
+app.use("/api/orders", require("./api/routes/orderRoute"));
+app.use("/api/users", require("./api/routes/userRoute"));
+app.use("/api/payments", require("./api/routes/paymentRoute"));
 
 // const server = app.listen(process.env.PORT, console.log(`Server running on port ${process.env.PORT}`.green.bold))
 
@@ -30,4 +39,4 @@ app.use('/api/payments', require('./api/routes/paymentRoute'))
 // 	server.close(() => process.exit(1));
 // })
 
-module.exports = app
+module.exports = app;
